36339b11f4b30a550a0bfdce44a519039bef30ba
Greg <2000247C@student.tp.edu.sg>
2021-12-11 21:22:03 +0800

    Made more progress on the fan and balloon piece

--- Assets/Creator Kit - Puzzle/Scenes/UserCreated/Scripts/Fan.cs
+++ Assets/Creator Kit - Puzzle/Scenes/UserCreated/Scripts/Fan.cs
@@ -7,6 +7,15 @@ public class Fan : MonoBehaviour
     public bool spin = false;
 
     private void Update(){
-        if(spin) transform.Rotate(270f*Time.deltaTime,0f,0f);
+        if (spin)
+        {
+            transform.Rotate(270f * Time.deltaTime, 0f, 0f);
+
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.right, out hit, 8f))
+            {
+                hit.rigidbody.AddForce(transform.right);
+            }
+        }
     }
 }
